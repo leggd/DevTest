d88a282710f0665d9e1fc64a574797b14709b23c
Daniel <d3764716@live.tees.ac.uk>
2024-01-31 18:44:52 +0000

    Testing VSC Integration

---
+++ pytest.py
@@ -0,0 +1,6 @@
+print("Hello, just testing Github integration with VSC. Is it working?")
+answer = input("Y/N?: ")
+if answer == "Y" or answer == "y":
+    print("Great!")
+else:
+    print("That's not good.")n
