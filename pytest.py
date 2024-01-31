0171e60918027e1a963f46dfa1059217ce20dc6e
leggd <158085318+leggd@users.noreply.github.com>
2024-01-31 18:59:15 +0000

    Delete pytest.py

--- pytest.py
+++
@@ -1,6 +0,0 @@
-print("Hello, just testing Github integration with VSC. Is it working?")
-answer = input("Y/N?: ")
-if answer == "Y" or answer == "y":
-    print("Great!")
-else:
-    print("That's not good.")n
